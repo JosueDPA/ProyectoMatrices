3e74e14a7157b8a701ecd969495d3a26ee48cfbb
JosueDPA <90888468+JosueDPA@users.noreply.github.com>
2021-11-02 17:17:35 -0700

    actualizacion para estetica 2

--- main.cpp
+++ main.cpp
@@ -13,35 +13,40 @@ int main(){
 	cin>>fila;
 	columna=fila;
 	cout<<endl;
-	cout<<"1-si desea multiplicar la matriz por un escalar "<<endl;
-	cout<<"2-si desea sumar dos matrices "<<endl;
-	cout<<"3-si desea multiplicar dos matrices "<<endl;
-	cout<<"4-si desea reducir la matriz por medio de gauss jordan"<<endl;
+	cout<<"1)   si desea multiplicar la matriz por un escalar "<<endl;
+	cout<<"2)   si desea sumar dos matrices "<<endl;
+	cout<<"3)   si desea multiplicar dos matrices "<<endl;
+	cout<<"4)   si desea reducir la matriz por medio de gauss jordan"<<endl;
 	Matrices Matriz1(fila,columna);
 	Matrices Matriz2(fila,columna);
 	Matrices Matriz3(fila,columna);
 	cin>>a;
 	switch(a){
 		case 1:
-		    Matriz1.ingresarM(0);
-		    cout<<"ingrese el escalar que se desea multiplicar "<<endl;
-		    cin>>Escalar;
-		    cout<<"Este seria el resultado de la multiplicacion"<<endl;
-		    Matriz1.MultiEs(Escalar);
+		     Matriz1.ingresarM(0);
+		     cout<<"ingrese el escalar que se desea multiplicar "<<endl;
+		     cin>>Escalar;
+		     cout<<"Este seria el resultado de la multiplicacion"<<endl;
+		     Matriz1.MultiEs(Escalar);
+		     return 0;
+		    
 		case 2:
-			Matriz1.ingresarM(0);
-			Matriz2.ingresarM(fila+2);
-			cout<<"el resultado de esta suma es "<<endl;
-			Matriz3.suma(Matriz1,Matriz2);
+			   Matriz1.ingresarM(0);
+			   Matriz2.ingresarM(fila+2);
+			   cout<<"el resultado de esta suma es "<<endl;
+			   Matriz3.suma(Matriz1,Matriz2);
+			   return 0;
 		case 3:
-			Matriz1.ingresarM(0);
-			Matriz2.ingresarM(fila+2);
-			cout<<"el resultado de la multiplicacion  de las matrices es "<<endl;
-	        Matriz3.Producto(Matriz1,Matriz2);
+			    Matriz1.ingresarM(0);
+			    Matriz2.ingresarM(fila+2);
+			    cout<<"el resultado de la multiplicacion  de las matrices es "<<endl;
+	            Matriz3.Producto(Matriz1,Matriz2);
+	            return 0;
 		case 4:
-		    Matriz1.ingresarM(0);
-			cout<<"se realiza el proceso de reduccion "<<endl;
-			Matriz1.Gaus();
+		        Matriz1.ingresarM(0);
+			    cout<<"se realiza el proceso de reduccion "<<endl;
+			    Matriz1.Gaus();
+			    return 0;
 	}
 	
 	return 0;
